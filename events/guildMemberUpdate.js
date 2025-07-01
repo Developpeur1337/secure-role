6213124896192d201409c19f986ee0b2f32027f2
Developpeur1337 <212281302+Developpeur1337@users.noreply.github.com>
2025-07-01 02:26:32 +0200

    Update guildMemberUpdate.js

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -1,14 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const {
-  AuditLogEvent,
-  EmbedBuilder,
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle,
-  ComponentType,
-} = require("discord.js");
-
+const { AuditLogEvent, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType } = require("discord.js");
 const dbPath = path.join(__dirname, "../db/db.json");
 
 module.exports = {
